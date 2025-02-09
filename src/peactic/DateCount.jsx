65d3401e4cb1d3d88b39615a95ad3a04efcc71e6
EylonMaayan <56929518+eylommaayan@users.noreply.github.com>
2025-02-09 21:07:46 +0200

    הוספת אתגר כפתור

---
+++ src/peactic/DateCount.jsx
@@ -0,0 +1,9 @@
+function DateCount() {
+    return (
+      <div style={{ color: "black", fontSize: "24px", fontWeight: "bold" }}>
+        Date Count Component
+      </div>
+    );
+  }
+  
+  export default DateCount;
